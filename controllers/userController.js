7b842ee625438ca6226bf7411b28b238079abec5
dream-team-alpha <dev@dreamreflectionmedia.com>
2024-10-14 14:23:56 +0530

    backend update getusers by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,8 +39,25 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createUser,
   getAllUsers,
+  getUserById,
   setSocketInstance
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createUser, getAllUsers } = require('../controllers/userController');
+const { createUser, getAllUsers, getUserById } = require('../controllers/userController');
 
 const router = express.Router();
 
 
 router.post('/', createUser);  // Handles POST requests to /api/users
 router.get('/', getAllUsers);  // Handles GET requests to /api/users
+router.get('/:id', getUserById);
 
 module.exports = router;
